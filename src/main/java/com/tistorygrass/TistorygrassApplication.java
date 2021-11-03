8e3fd20eb80e0aceb877974928f850348da8c0a0
kujaHn <72388950+kujaHn@users.noreply.github.com>
2021-11-03 21:40:04 +0900

    Initial commit

---
+++ src/main/java/com/tistorygrass/TistorygrassApplication.java
@@ -0,0 +1,13 @@
+package com.tistorygrass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TistorygrassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TistorygrassApplication.class, args);
+    }
+
+}
